7ddf9c39e01aa5eba2d44bcb94b68e70522aabc1
Janhavi Nagda <janhavinagda@gmail.com>
2025-07-30 23:11:54 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1s ease-out',
+        'bounce-slow': 'bounce 3s infinite',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
